55de5e1dd0bb78a8ee3c5fb48af58e9b2236ca46
Divyansh Gupta <32055284+whatisdivdoing@users.noreply.github.com>
2024-10-18 17:08:02 +0530

    Update 01 Update-table.sql

--- 01 Update-table.sql
+++ 01 Update-table.sql
@@ -1,24 +1,3 @@
--- Adding "duration" column in the monthly tables
-ALTER TABLE divvy.mar2023
-ADD COLUMN duration BIGINT AFTER ended_at;
-
--- Setting "duration" column in seconds
-UPDATE divvy.mar2023 
-SET duration = TIME_TO_SEC(TIMEDIFF(ended_at, started_at));
--- .........................................................
-
--- Renaming a column
-ALTER TABLE divvy.year2023 
-RENAME COLUMN ride_length TO duration; 
--- ........................................
-
-/* Setting the duration column for the year table
- * Since MySQL treats warnings as Errors, it won't populate the table unless the "IGNORE" keyword is used. 
- * */
-UPDATE IGNORE divvy.year2023 
-SET duration = COALESCE(TIME_TO_SEC(TIMEDIFF(ended_at, started_at)),0); 
--- ..................................................................................................................
-
 /* Code for creating the column "ride_length" in the year2023 table
  * ride_length stores the duration of the rides in "TIME" dataype with the HH:MM:SS format
  * It happens in 2 steps : 
@@ -33,6 +12,20 @@ SET duration = COALESCE(TIME_TO_SEC(TIMEDIFF(ended_at, started_at)),0);
 	SET ride_length = TIMEDIFF(ended_at,started_at);
 -- ..................................................................................................................
 
+/* Code for creating the column "duration" in the year2023 table
+ * duration stores the duration of the rides in seconds as "BIGINT" dataype */
+
+	-- Creating the column using "ADD" keyword
+	ALTER TABLE divvy.year2023 
+	ADD COLUMN duration BIGINT AFTER ride_length
+/* Setting the duration column for the year table
+ * Since MySQL treats warnings as Errors, it won't populate the table unless the "IGNORE" keyword is used. 
+ * */
+UPDATE IGNORE divvy.year2023 
+SET duration = TIME_TO_SEC(TIMEDIFF(ended_at, started_at)); 
+-- ..................................................................................................................
+
+
 /* Code for creating the COLUMN "day_of_week" which stores the day of the week
  * Data type would be VARCHAR(16)
  */
@@ -43,7 +36,3 @@ ADD COLUMN day_of_week VARCHAR(10) AFTER duration
 UPDATE divvy.year2023 
 SET day_of_week = DAYNAME(started_at);  
 -- ..................................................................................................................
-
-/* Code for creating the column "start_time" which stores the start time of the rides
- * 
- */
